4f7e0309426b4ae22e2a3ef3fbf1f71e66db70d0
Anggerdhismakusuma <kangger97@gmail.com>
2025-05-14 16:13:23 +0700

    Daily Commit

---
+++ Exercises/Heaps.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#define MAX 100
+
+typedef struct MinHeap{
+    int size;
+    int arr[MAX];
+}MinHeap;
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int parent(int i){
+    return (i - 1) / 2;
+}
+
+int leftChild(int i){
+    return i * 2 + 1;
+}
+
+int rightChild(int i){
+    return i * 2 + 2;
+}
+
+void insert(MinHeap *heap, int value){
+    if(heap->size == MAX){
+        printf("Full\n");
+        return;
+    }
+
+    int i = heap->size;
+    heap->arr[i] = value;
+    heap->size++;
+
+    while(i != 0 && heap->arr[parent(i)] > heap->arr[i]){
+        swap(&heap->arr[parent(i)], &heap->arr[i]);
+        i = parent(i);
+    }
+
+}
+
+void DownHeap(MinHeap *heap, int i){
+    int smallest = i;
+    int left = leftChild(i);
+    int right = rightChild(i);
+
+    if(left < heap->size && heap->arr[left] < heap->arr[i]){
+        smallest = left;
+    }
+
+    if(right < heap->size && heap->arr[right] < heap->arr[i]){
+        smallest = right;
+    }
+
+    if(smallest != i){
+        swap(&heap->arr[i], &heap->arr[smallest]);
+        DownHeap(heap, smallest);
+    }
+}
+
+int extract(MinHeap* heap){
+    if(heap->size <= 0){
+        printf("Empty\n");
+        return -1;
+    }
+
+    int root = heap-> arr[0];
+    heap->arr[0] = heap->arr[heap->size - 1];
+    heap->size--;
+
+    DownHeap(heap, 0);
+    return root;
+}
+
+
+void printHeap(MinHeap* heap){
+    int j = 0;
+    while(j < heap->size){
+        printf("%d ", heap->arr[j]);
+        j++;
+    }
+    printf("\n");
+}
+
+int main(){
+    MinHeap heap;
+    heap.size = 0;
+
+    insert(&heap, 10);
+    insert(&heap, 5);
+    insert(&heap, 20);
+    insert(&heap, 3);
+
+    printHeap(&heap);
+
+    extract(&heap);
+
+    printHeap(&heap);
+    return 0;
+}
--- LAB/Graph.cpp
+++ LAB/Graph.cpp
@@ -6,8 +6,66 @@
 
 void createMatrix(int matrix[][MAX], int vertices){
     for(int i = 0; i < vertices; i++){
-        for(int j = 0; i <vertices; j++){
+        for(int j = 0; j < vertices; j++){
             matrix[i][j] = 0;
+        }
+    }
+}
+
+void addEdgeMatrix(int matrix[][MAX], int src, int dest){
+    matrix[src][dest] = 1;
+    matrix[dest][src] = 1;
+}
+
+void bfsMatrix(int matrix[][MAX], int vertices, int startVertex){
+    //Step 1: Buat array untuk menyimpan status kunjungan vertex
+    // dan inisialisasi semua vertex sebagai belum dikunjungi
+    bool visited[vertices];
+    for(int i = 0; i < vertices; i++){
+        visited[i] = false; // belum ada vertex yang dikunjungi
+    }
+
+    // Step 2: Buat antrian untuk BFS
+    int queue[vertices], front = -1, rear = -1;
+
+    visited[startVertex] = true; // tandai vertex awal sebagai dikunjungi
+    queue[rear++] = startVertex; // Masukkin vertex awal ke antrian
+    
+    // kita pengen looping BFS
+    while(front < rear){
+        int currentVertex = queue[front++];
+        printf("%d ", currentVertex);
+
+        // kita pengen explore vertex tersebut punya neighbors
+        for(int i = 0; i < vertices; i++){
+            if(matrix[currentVertex][i] == 1 && !visited[i]){
+                visited[i] = true;
+                queue[rear++] = i; // enqueue vertex yang belum dikunjungi
+            }
+        }
+    }
+}
+
+void dfsMatrix(int matrix[][MAX], int vertices, int startVertex){
+    bool visited[vertices]; // tandai vertex sebagai dikunjungi
+    for(int i = 0; i < vertices; i++){
+        visited[i] = false; // belum ada vertex yang dikunjungi
+    }
+
+    int stack[150], top = -1; // buat stack untuk DFS
+    stack[++top] = startVertex; // masukkan vertex awal ke stack
+    while(top >= 0){
+        int currentVertex = stack[top--]; // ambil vertex dari stack
+        if(!visited[currentVertex]){
+            printf("%d ", currentVertex); // cetak vertex
+            visited[currentVertex] = true; // tandai sebagai dikunjungi
+        }
+
+        // visit semua neighbors
+        for(int i = vertices - 1; i >= 0; i--){
+            if(matrix[currentVertex][i] == 1 && !visited[i]){
+                stack[++top] = i; // masukkan neighbor ke stack
+            }
         }
     }
 }
@@ -22,6 +80,25 @@ void printMatrix(int matrix[][MAX], int vertices){
 }
 
 int main(){
+    int vertices = MAX;
+    int matrix[MAX][MAX];
+    
+    createMatrix(matrix, vertices);
+
+    addEdgeMatrix(matrix, 0, 1);
+    addEdgeMatrix(matrix, 0, 2);
+    addEdgeMatrix(matrix, 1, 2);
+    addEdgeMatrix(matrix, 1, 3);
+    addEdgeMatrix(matrix, 2, 4);
+
+    puts("Matrix Adjacency:");
+    printMatrix(matrix, vertices);
+    
+    puts("\nBFS Matrix:");
+    bfsMatrix(matrix, vertices, 0); // Mulai dari vertex 0
+
+    puts("\n\nDFS Matrix:");
+    dfsMatrix(matrix, vertices, 0); // Mulai dari vertex 0
 
     return 0;
 }
